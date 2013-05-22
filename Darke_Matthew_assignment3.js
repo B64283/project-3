ce5dd349dafc78f6a492f2ac276c081013f816a3
B64283 <mattdarke@fullsail.edu>
2013-05-21 21:49:54 -0700

    nested conditionals

--- Darke_Matthew_assignment3.js
+++ Darke_Matthew_assignment3.js
@@ -14,27 +14,26 @@ var SheWins = true
 // Procedure 
 function eatingcontest(askAllowance) {
      if (askAllowance === true) { 
-	     console.log(" This year " + KidsName + " is going to give it another go at the " + carnivalName + " to try to beat her last years records .\n but she needs to ask Dad for her allowance to buy a ticket for each entry. ")
+	     console.log(" This year " + KidsName + " is going to give it another go at the " + carnivalName + " eating contest to try to beat her last years records .\n but she needs to ask Dad for her allowance to buy a ticket for each entry. ")
      } else { 
          console.log (" Darn, theres always next year for " + KidsName + " to try to beat her record. ") 
-	     }
+	 }
 };	     
-
+eatingcontest(askAllowance);
 //number arg and math
 var buyTickets = function(givenAllowance) {
 	var PricePerTicket = 18,
 	    wholeTickets;
 	if (givenAllowance < PricePerTicket) {
-	    console.log("Thats not enough to buy all three tickets!");
+	    console.log("Dad, thats not enough to buy all three tickets!");
 	    return 0;
   } else {
 	    wholeTickets = Math.floor(givenAllowance / PricePerTicket);
-    } 
-    return wholeTickets;
+        return wholeTickets;   
+  }
 };
-var gotTickets = buyTickets(54);
-console.log("Thanks for giving me enough to buy" + gotTickets + "Tickets");
-
+var gotTickets = buyTickets(55);//use console.log below at end main code
+console.log(" Thanks Dad for giving me enough to buy all " + gotTickets + " Tickets! ");
 
 
 
